0ee6110746d5369526ad9bb9fd6491d2ede43656
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2025-10-15 15:16:45 +0600

    bug fixed

--- src/app/(dashboard)/admin/layout.js
+++ src/app/(dashboard)/admin/layout.js
@@ -10,7 +10,7 @@ const DashboardLayout = ({ children }) => {
   return (
     <section className="flex ">
       <DashboardSidebar />
-      <div className="grow px-[24px] py-[22.5px]">
+      <div className="grow px-[24px] py-[22.5px] flex flex-col gap-[34px]">
         <DashboardTopbar />
         {children}
       </div>
