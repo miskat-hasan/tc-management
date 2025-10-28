e2d94face2af2938352de96ac775cb458e9044de
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2025-10-28 12:57:16 +0600

    instructor bidding responsive added

--- src/app/(dashboard)/admin/class_and_students/instructor_bidding/page.js
+++ src/app/(dashboard)/admin/class_and_students/instructor_bidding/page.js
@@ -1,14 +1,14 @@
 import SectionTitle from "@/components/common/SectionTitle";
 import NotFound from "@/components/shared/NotFound";
-import { InfoIcon } from "@/svg/SvgContainer";
+
 import React from "react";
 
 const page = () => {
   return (
-    <section className="w-full flex flex-col gap-[25px]">
+    <section className="w-full flex flex-col gap-[12.5px] lg:gap-[25px]">
       <SectionTitle title={"Instructor Bidding"} />
 
-      <div className="px-[24px] py-[24px] bg-white rounded-[16px] flex gap-[24px] w-full">
+      <div className="px-[12px] lg:px-[24px] py-[12px] lg:py-[24px] bg-white rounded-[16px] flex gap-[24px] w-full">
         <NotFound title={"No classes are current open for bidding"} />
       </div>
     </section>
--- src/components/shared/NotFound.jsx
+++ src/components/shared/NotFound.jsx
@@ -3,9 +3,13 @@ import React from "react";
 
 const NotFound = ({ title }) => {
   return (
-    <div className="p-[24px] bg-[#FFF3F3] border border-brown w-full rounded-[10px] flex items-center gap-[10px] font-medium">
-      <InfoIcon />
-      <p className="text-[18px] text-brown">{title || "No Records Found"}</p>
+    <div className="p-[8px] lg:p-[24px] bg-[#FFF3F3] border border-brown w-full rounded-[10px] flex items-center gap-[5px] lg:gap-[10px] font-medium">
+      <div className="shrink-0">
+        <InfoIcon />
+      </div>
+      <p className="text-[12px] lg:text-[18px] text-brown">
+        {title || "No Records Found"}
+      </p>
     </div>
   );
 };
