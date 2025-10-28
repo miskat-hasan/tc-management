f7c832540f3acd061c259822a95a2a9c3ac4da4c
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2025-10-28 17:34:43 +0600

    support request page responsive added

--- src/app/(dashboard)/admin/help/support_request/page.js
+++ src/app/(dashboard)/admin/help/support_request/page.js
@@ -6,7 +6,6 @@ import FormInput from "@/components/shared/form/FormInput";
 import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import React from "react";
-import CustomSelect from "@/components/shared/form/CustomSelect";
 import FormTextarea from "@/components/shared/form/FormTextarea";
 
 const Page = () => {
@@ -22,7 +21,7 @@ const Page = () => {
     <section className="flex flex-col gap-4">
       {/* Title */}
       <SectionTitle title="New Asset Location" />
-      <div className="bg-white rounded-[14px] p-8 shadow-sm">
+      <div className="bg-white rounded-[14px] p-4 lg:p-8 shadow-sm">
         <p className="text-gray-500">
           Have a te&lical issue or an enhancement request? You can send an emai
           to <b className="text-black">support@enrolhvare.com</b> or fill out
@@ -30,10 +29,10 @@ const Page = () => {
         </p>
       </div>
       {/* White Form Card */}
-      <div className="bg-white rounded-[14px] p-8 shadow-sm">
+      <div className="bg-white rounded-[14px] p-4 lg:p-8 shadow-sm">
         <FormContainer form={form} onSubmit={onSubmit}>
           {/* Grid Layout */}
-          <div className="grid grid-cols-2 gap-x-6 gap-y-5">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-6 gap-y-2 lg:gap-y-5">
             <FormInput name="name" label="Name" placeholder="Name here" />
 
             <FormInput
@@ -42,7 +41,7 @@ const Page = () => {
               placeholder="Mobile"
             />
 
-            <div className="col-span-full w-full flex flex-col gap-5">
+            <div className="col-span-full w-full flex flex-col gap-2 lg:gap-5">
               <FormInput
                 name="emailAddress"
                 label="Email Address"
@@ -57,7 +56,7 @@ const Page = () => {
           </div>
 
           {/* Footer Buttons */}
-          <div className="flex justify-end gap-4 mt-10">
+          <div className="flex justify-end gap-4 mt-5 lg:mt-10">
             <Button
               type="submit"
               className="px-6 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium cursor-pointer text-white bg-brown hover:bg-brown-hover"
