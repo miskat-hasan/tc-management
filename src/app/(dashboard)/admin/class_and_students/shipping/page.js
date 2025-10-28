7bc5de08d0473fa9cf0ea6517e652eb11675c49b
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2025-10-28 14:14:51 +0600

    shipping responsive added

--- src/app/(dashboard)/admin/class_and_students/shipping/page.js
+++ src/app/(dashboard)/admin/class_and_students/shipping/page.js
@@ -4,10 +4,9 @@ import React from "react";
 
 const page = () => {
   return (
-    <section className="w-full flex flex-col gap-[25px]">
+    <section className="w-full flex flex-col gap-[12.5px] lg:gap-[25px]">
       <SectionTitle title={"Shipping"} />
-
-      <div className="px-[24px] py-[24px] bg-white rounded-[16px] flex gap-[24px] w-full">
+      <div className="px-[16px] py-[16px] lg:px-[32px] lg:py-[32px] bg-white rounded-[16px] flex gap-[12px] lg:gap-[24px] w-full">
         <NotFound title={"No item require shipping at this time"} />
       </div>
     </section>
