f962de3bc6c287c512ef02ce6424cddf47bd3759
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2025-10-29 12:36:23 +0600

    edit email campaign responsive added

--- src/app/(dashboard)/admin/settings/emails_campaigns/[id]/edit_email_campaign/page.js
+++ src/app/(dashboard)/admin/settings/emails_campaigns/[id]/edit_email_campaign/page.js
@@ -6,7 +6,6 @@ import FormInput from "@/components/shared/form/FormInput";
 import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import React from "react";
-import CustomSelect from "@/components/shared/form/CustomSelect";
 
 const Page = () => {
   const form = useForm({
@@ -23,10 +22,10 @@ const Page = () => {
       <SectionTitle title="Edit Email Campaigns" />
 
       {/* White Form Card */}
-      <div className="bg-white rounded-[14px] p-8 shadow-sm">
+      <div className="bg-white rounded-[14px] p-4 lg:p-8 ">
         <FormContainer form={form} onSubmit={onSubmit}>
           {/* Grid Layout */}
-          <div className="grid grid-cols-2 gap-x-6 gap-y-5">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-3 gap-y-2.5 lg:gap-x-6 lg:gap-y-5">
             <FormInput name="Name" label="Name" placeholder="Name here" />
 
             <FormInput
@@ -48,14 +47,14 @@ const Page = () => {
           </div>
 
           {/* Options + Roles */}
-          <div className="grid grid-cols-2 gap-10 mt-8">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 lg:gap-10 mt-4 lg:mt-8">
             <div className="flex flex-col gap-2">
               <p className="font-semibold text-[15px] text-gray-700">Options</p>
-              <label className="flex items-center gap-2 text-sm">
+              <label className="flex items-center gap-2 text-[10px] lg:text-sm">
                 <input type="checkbox" className="accent-brown" />
                 Active
               </label>
-              <label className="flex items-center gap-2 text-sm">
+              <label className="flex items-center gap-2 text-[10px] lg:text-sm">
                 <input type="checkbox" className="accent-brown" />
                 Stop this campaign if the customer registers for a renewal class
               </label>
@@ -65,11 +64,11 @@ const Page = () => {
             <p className="font-semibold text-[15px] text-gray-700">
               Send to all Course Types
             </p>
-            <label className="flex items-center gap-2 text-sm">
+            <label className="flex items-center gap-2 text-[10px] lg:text-sm">
               <input type="checkbox" className="accent-brown" />
               Yes
             </label>
-            <label className="flex items-center gap-2 text-sm">
+            <label className="flex items-center gap-2 text-[10px] lg:text-sm">
               <input type="checkbox" className="accent-brown" />
               No
             </label>
@@ -79,45 +78,45 @@ const Page = () => {
             <p className="font-semibold text-[15px] text-gray-700">
               Courses to Send
             </p>
-            <label className="flex items-center gap-2 text-sm">
+            <label className="flex items-center gap-2 text-[10px] lg:text-sm">
               <input type="checkbox" className="accent-brown" />
               Life Threatening Bleeding and Tourniquet Application
             </label>
-            <label className="flex items-center gap-2 text-sm">
+            <label className="flex items-center gap-2 text-[10px] lg:text-sm">
               <input type="checkbox" className="accent-brown" />
               (American Heart Association) ACLS INSTRUCTOR TRAINING COURSE
             </label>
-            <label className="flex items-center gap-2 text-sm">
+            <label className="flex items-center gap-2 text-[10px] lg:text-sm">
               <input type="checkbox" className="accent-brown" />
               (American Heart Association) Advanced Stroke Life Support Blended
               In-Hospital and Pre-Hospital Provider Course
             </label>
-            <label className="flex items-center gap-2 text-sm">
+            <label className="flex items-center gap-2 text-[10px] lg:text-sm">
               <input type="checkbox" className="accent-brown" />
               (American Heart Association) Advanced Stroke Life Support Blended
               In-Hospital and Pre-Hospital Provider Course
             </label>
-            <label className="flex items-center gap-2 text-sm">
+            <label className="flex items-center gap-2 text-[10px] lg:text-sm">
               <input type="checkbox" className="accent-brown" />
               (American Heart Association) Advanced Stroke Life Support Blended
               In-Hospital and Pre-Hospital Provider Course
             </label>
-            <label className="flex items-center gap-2 text-sm">
+            <label className="flex items-center gap-2 text-[10px] lg:text-sm">
               <input type="checkbox" className="accent-brown" />
               (American Heart Association) Advanced Stroke Life Support Blended
               In-Hospital and Pre-Hospital Provider Course
             </label>
-            <label className="flex items-center gap-2 text-sm">
+            <label className="flex items-center gap-2 text-[10px] lg:text-sm">
               <input type="checkbox" className="accent-brown" />
               (American Heart Association) Advanced Stroke Life Support Blended
               In-Hospital and Pre-Hospital Provider Course
             </label>
-            <label className="flex items-center gap-2 text-sm">
+            <label className="flex items-center gap-2 text-[10px] lg:text-sm">
               <input type="checkbox" className="accent-brown" />
               (American Heart Association) Advanced Stroke Life Support Blended
               In-Hospital and Pre-Hospital Provider Course
             </label>
-            <label className="flex items-center gap-2 text-sm">
+            <label className="flex items-center gap-2 text-[10px] lg:text-sm">
               <input type="checkbox" className="accent-brown" />
               (American Heart Association) Advanced Stroke Life Support Blended
               In-Hospital and Pre-Hospital Provider Course
@@ -125,16 +124,16 @@ const Page = () => {
           </div>
 
           {/* Footer Buttons */}
-          <div className="flex justify-end gap-4 mt-10">
+          <div className="flex justify-end gap-4 mt-5 lg:mt-10">
             <Button
               type="button"
-              className="px-6 py-2 bg-transparent border border-gray-300 rounded-md text-sm font-medium text-black hover:bg-gray-50"
+              className="px-3 lg:px-6 py-2 bg-transparent border border-gray-300 rounded-md text-[10px] lg:text-sm font-medium text-black hover:bg-gray-50"
             >
               Back
             </Button>
             <Button
               type="submit"
-              className="px-6 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium cursor-pointer text-white bg-brown hover:bg-brown-hover"
+              className="px-3 lg:px-6 py-2 border border-transparent rounded-md shadow-sm text-[10px] lg:text-sm font-medium cursor-pointer text-white bg-brown hover:bg-brown-hover"
             >
               Save Changes
             </Button>
