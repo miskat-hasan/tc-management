13762aed02e43b72a80e5613e379cf4a814c2b87
Miskat Hasan <miskathasan979@gmail.com>
2026-01-05 17:57:48 +0600

    fixed/ main page auth settings

--- src/app/page.js
+++ src/app/page.js
@@ -3,4 +3,11 @@
 import useAuth from "@/hooks/useAuth";
 import { redirect } from "next/navigation";
 
-export default function Home() {<></>}
+export default function Home() {
+const {token,user} = useAuth();
+ if(token && user) {
+  redirect("/admin/class_and_students/upcoming_classes")
+ }else{
+  redirect("/login")
+ }
+}
