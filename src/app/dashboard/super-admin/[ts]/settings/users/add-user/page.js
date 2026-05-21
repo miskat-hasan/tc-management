3ddebcf66da36486b4785ea20df48da22d96f0af
Miskat Hasan <miskathasan979@gmail.com>
2026-05-21 17:03:17 +0600

    feat/ designed & implemented add user (for ts 'admin' dashboard)

---
+++ src/app/dashboard/super-admin/[ts]/settings/users/add-user/page.js
@@ -0,0 +1,11 @@
+import AddUser from "@/components/dashboard/settings/AddUser"
+
+const AddUserPage = () => {
+  return (
+    <div>
+      <AddUser />
+    </div>
+  )
+}
+
+export default AddUserPage
